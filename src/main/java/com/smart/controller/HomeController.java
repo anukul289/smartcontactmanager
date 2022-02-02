d288f6969a66e843cd632699b98095e65f6c10c5
ANUKUL OJHA <anukul289@gmail.com>
2022-02-02 11:10:16 +0530

    Bootstrap and Template setup

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,26 +1,24 @@
 package com.smart.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.smart.dao.UserRepository;
-import com.smart.entities.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private UserRepository userRepository;
 
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		User user = new User();
-		user.setName("Anukul Ojha");
-		user.setEmail("anukul299@gmail.con");
-		userRepository.save(user);
-		return "working";
+	@RequestMapping("/home")
+	public String home(Model m)
+	{
+		m.addAttribute("title", "Home - Smart Contact Manager");
+		return "home";
+	}
+	
+	
+	@RequestMapping("/about")
+	public String about(Model m)
+	{
+		m.addAttribute("title", "About - Smart Contact Manager");
+		return "about";
 	}
 }
